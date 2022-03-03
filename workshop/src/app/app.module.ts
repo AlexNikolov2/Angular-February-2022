9e4f7813c63a0374b750fd00f66f76a83163a77a
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-03 11:22:05 +0200

    workshop 01 done

--- workshop/src/app/app.module.ts
+++ workshop/src/app/app.module.ts
@@ -1,8 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentService } from './content.service';
 import { CoreModule } from './core/core.module';
 import { ThemeComponent } from './theme/theme.component';
 
@@ -16,7 +18,9 @@ import { ThemeComponent } from './theme/theme.component';
     AppRoutingModule,
     CoreModule
   ],
-  providers: [],
+  providers: [
+    ContentService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
