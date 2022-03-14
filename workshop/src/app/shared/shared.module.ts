3eb7742c850bb799c219e27f889ab1b06f2823ef
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-14 19:50:46 +0200

    build

--- workshop/src/app/shared/shared.module.ts
+++ workshop/src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeMessageComponent } from './welcome-message/welcome-message.component';
 import { RouterModule } from '@angular/router';
+import { CustomValidatorDirective } from './custom-validator.directive';
 
 
 
 @NgModule({
   declarations: [
-    WelcomeMessageComponent
+    WelcomeMessageComponent,
+    CustomValidatorDirective
   ],
   imports: [
     CommonModule,
