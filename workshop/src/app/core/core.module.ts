b353368559917c938628247d24f31b77fad200e4
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-16 18:27:02 +0200

    build 04

--- workshop/src/app/core/core.module.ts
+++ workshop/src/app/core/core.module.ts
@@ -1,11 +1,15 @@
 import { NgModule, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { LocalStorage } from './injection-tokens';
 import { AuthActivate } from './guards/auth.activate';
+import { appInterceptorProvider } from './app-interceptor';
+import { globalErrorHandlerProvider } from './error-handler';
+import { UserService } from './services/user.service';
+import { ContentService } from './services/content.service';
 
 @NgModule({
   declarations: [
@@ -15,52 +19,57 @@ import { AuthActivate } from './guards/auth.activate';
   imports: [
     CommonModule,
     RouterModule,
-    HttpClientModule,
+    HttpClientModule
   ],
   exports: [
     HeaderComponent,
     FooterComponent
   ],
+  providers: [
+    {
+      provide: LocalStorage,
+      useFactory: (platformId: Object) => {
 
-  providers: [ {
-    provide: LocalStorage,
-    useFactory: (platformId: Object) => {
+        if (isPlatformBrowser(platformId)) {
+          return window.localStorage;
+        }
+        if (isPlatformServer(platformId)) {
+          return class implements Storage {
+            length = 0;
+            private data: Record<string, string> = {};
 
-      if (isPlatformBrowser(platformId)) {
-        return window.localStorage;
-      }
-      if (isPlatformServer(platformId)) {
-        return class implements Storage {
-          length = 0;
-          private data: Record<string, string> = {};
+            clear(): void {
+              this.data = {};
+            }
 
-          clear(): void {
-            this.data = {};
-          }
+            getItem(key: string): string | null {
+              return this.data[key];
+            }
 
-          getItem(key: string): string | null {
-            return this.data[key];
-          }
+            key(index: number): string | null {
+              throw new Error('Method not implemented.');
+            }
 
-          key(index: number): string | null {
-            throw new Error('Method not implemented.');
-          }
+            removeItem(key: string): void {
+              const { [key]: removedItem, ...others } = this.data;
+              this.data = others;
+            }
 
-          removeItem(key: string): void {
-            const { [key]: removedItem, ...others } = this.data;
-            this.data = others;
-          }
-
-          setItem(key: string, value: string): void {
-            this.data[key] = value;
+            setItem(key: string, value: string): void {
+              this.data[key] = value;
+            }
           }
         }
-      }
-      throw Error('NOT IMPLEMENTED');
+        throw Error('NOT IMPLEMENTED');
+      },
+      deps: [PLATFORM_ID]
     },
-    deps: [PLATFORM_ID]
-  },
-  AuthActivate
-]
+    AuthActivate,
+    appInterceptorProvider,
+    globalErrorHandlerProvider,
+    UserService,
+    ContentService
+  ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
---
+++ workshop/src/app/core/error-handler.ts
@@ -0,0 +1,18 @@
+import { ErrorHandler, Injectable, Provider } from "@angular/core";
+import { Router } from "@angular/router";
+
+class GlobalErrorHandler implements ErrorHandler {
+
+  constructor(private router: Router) { }
+
+  handleError(error: Error) {
+    console.log(error);
+    this.router.navigate(['/error'], { queryParams: { error: 'Ooops! Stana fal!' } });
+  }
+}
+
+export const globalErrorHandlerProvider: Provider = {
+  provide: ErrorHandler,
+  useFactory: (router: Router) => new GlobalErrorHandler(router),
+  deps: [Router]
+};
